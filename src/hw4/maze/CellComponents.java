1e3e6dafd531c345c69696e3349551c6fd61252a
JerseySchunk <jjsprq@umsystem.edu>
2025-04-18 16:31:24 -0500

    Added javadocs

--- src/hw4/maze/CellComponents.java
+++ src/hw4/maze/CellComponents.java
@@ -1,6 +1,13 @@
 package hw4.maze;
 
-
+/**
+ * An enumeration of possible components on each side of a cell within the maze.
+ * 
+ * The valid components are:
+ * @code WALL - Solid barrier, the agent cannot move through
+ * @code APERTURE - Open path the agent can travel through
+ * @code EXIT - Special escape point on the left of a leftmost cell
+ */
 
 public enum CellComponents {
 	WALL,
