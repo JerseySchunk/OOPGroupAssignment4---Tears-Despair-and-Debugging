719957572fdd490cdfd965cb30c684d90426d011
JerseySchunk <jjsprq@umsystem.edu>
2025-04-18 17:20:03 -0500

    Added Javadocs

--- src/hw4/maze/Grid.java
+++ src/hw4/maze/Grid.java
@@ -2,6 +2,20 @@ package hw4.maze;
 
 import java.util.ArrayList;
 
+/**
+ * Represents the entire maze structure used in TDD
+ * 
+ * A grid is made up of multiple {@link Row} objects, and each row
+ * contains several {@link Cell} objects. Together, they form a 2D maze that
+ * the player can move through.
+ *
+ *Provides methods to access and update the grid's layout.
+ *It also includes readable string formats of the grid.
+ *
+ *@author
+ *	Jersey Schunk
+ */
+
 public class Grid {
 	private ArrayList<Row> rows;
 	
