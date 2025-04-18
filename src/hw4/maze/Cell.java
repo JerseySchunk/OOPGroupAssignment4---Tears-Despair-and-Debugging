8202b2a2e3c7f7ac0785e06ebb35bc9daf400da3
JerseySchunk <jjsprq@umsystem.edu>
2025-04-18 16:57:28 -0500

    Initialization and constructor

--- src/hw4/maze/Cell.java
+++ src/hw4/maze/Cell.java
@@ -2,4 +2,39 @@ package hw4.maze;
 
 public class Cell {
 
+	private CellComponents up;
+	private CellComponents down;
+	private CellComponents left;
+	private CellComponents right;
+	
+	public Cell(CellComponents up, CellComponents down, CellComponents left, CellComponents right){
+		
+		if(left != null) {
+			this.left = left;
+		}else {
+			this.left = CellComponents.WALL;
+		}
+		
+		if(right != null) {
+			this.right = right;
+		}else {
+			this.right = CellComponents.WALL;
+		}
+		
+		if(up != null) {
+			this.up = up;
+		}else {
+			this.up = CellComponents.WALL;
+		}
+		
+		if(down != null){
+			this.down = down;
+		}else {
+			this.down = CellComponents.WALL;
+		}
+		
+	}
+	
+	
+	
 }
