8bf9d84a0364b635f40b51c254d14474af82fe69
Gideon <gideonmasters09876@gmail.com>
2025-04-21 12:35:51 -0700

    Imported Cell, Grid,etc and getting grid positions

--- src/hw4/player/Player.java
+++ src/hw4/player/Player.java
@@ -1,5 +1,13 @@
 package hw4.player;
 
+import hw4.maze.Cell;
+import hw4.maze.Grid;
+import hw4.maze.CellComponents;
+import hw4.maze.Row;
+
 public class Player {
 
+	//Stores the position of the player in the grid
+	private Row currentRow;
+	private Cell currentCell;
 }
