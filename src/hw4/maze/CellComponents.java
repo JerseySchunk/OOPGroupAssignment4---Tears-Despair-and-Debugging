cd0e679815b05d0628dc6defccdee0a7ce3a1e6a
JerseySchunk <jjsprq@umsystem.edu>
2025-04-21 11:39:35 -0500

    Updated Javadocs

--- src/hw4/maze/CellComponents.java
+++ src/hw4/maze/CellComponents.java
@@ -3,6 +3,8 @@ package hw4.maze;
 /**
  * An enumeration of possible components on each side of a cell within the maze.
  * 
+ * @author Jersey Schunk
+ * 
  * The valid components are:
  * @code WALL - Solid barrier, the agent cannot move through
  * @code APERTURE - Open path the agent can travel through
