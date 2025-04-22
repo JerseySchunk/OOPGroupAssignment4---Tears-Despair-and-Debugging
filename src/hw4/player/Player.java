fe24275f8feaf10a5ed8db99c330f0bafcf1d09a
Son <Son@MSI>
2025-04-22 17:36:53 -0500

    had to change the logic in Player.java to fix a bug in game.java

--- src/hw4/player/Player.java
+++ src/hw4/player/Player.java
@@ -1,17 +1,16 @@
 package hw4.player;
 
 import hw4.maze.Cell;
+import hw4.maze.Grid;
 import hw4.maze.CellComponents;
 import hw4.maze.Row;
 
 public class Player {
 
-	//Stores the position of the player in the grid
+	// Stores the position of the player in the grid
 	private Row currentRow;
 	private Cell currentCell;
 
-	
-	//
 	public Player(Row currentRow, Cell currentCell) {
 		this.currentRow = currentRow;
 		this.currentCell = currentCell;
@@ -27,89 +26,68 @@ public class Player {
 
 	/**
 	 * Moves the player using a boolean value to see if something is blocking the movement.
-	 * 
+	 *
 	 * 4 types of movement options
 	 * UP, DOWN, LEFT, RIGHT
 	 * example use Movement.LEFT
 	 * @return true if the move was possible otherwise false
 	 */
-	public boolean move(Movement movement) {
-		
+	public boolean move(Movement direction, Grid grid) {
+
 		boolean moved = false;
 		int currentIndex = currentRow.getCells().indexOf(currentCell);
-		
-		switch (movement) {
-		/**
-		 * checks the position of the player to see if it's on the very top
-		 * If it is on the very top it can't move more up
-		 * If not it can move up by moving up one on the index which is - 1
-		 */			
-		case UP:
-			if (currentRow.getCells().indexOf(currentCell) > 0) {
-				Cell aboveCell = currentRow.getCells().get(currentIndex - 1);
-				if (aboveCell.getUp() != CellComponents.WALL) {
-					currentCell = aboveCell;
-					moved = true;
-				}
-			}
-			break;
+		int rowIndex = grid.getRows().indexOf(currentRow);
 
-			/**
-			 * checks the position of the player to see if it's on the very bottom
-			 * If it is on the very bottom
-			 * If not it can move down by moving down one on the index which is + 1
-			 */		
-		case DOWN:
-			if(currentRow.getCells().indexOf(currentCell) < currentRow.getCells().size() - 1) {
-				Cell belowCell = currentRow.getCells().get(currentIndex + 1);
-				if (belowCell.getDown() != CellComponents.WALL) {
-					currentCell = belowCell;
-					moved = true;
-				}				
-			}
-			
-			
-			
-		/**
-		 * Checks if the left of the Cell has a space
-		 * Then it checks the position of the player to see if it's on the very left
-		 * If it is on the very left wall it can't more left again
-		 * If not it can move left by moving down one on the index
-		 */
-		case LEFT:
-			if (currentCell.getLeft() != CellComponents.WALL) {
-				if(currentIndex > 0) {
-					currentCell = currentRow.getCells().get(currentIndex -1);
+		switch (direction) {
+			case UP:
+				if (rowIndex > 0) {
+					Row aboveRow = grid.getRows().get(rowIndex - 1);
+					Cell aboveCell = aboveRow.getCells().get(currentIndex);
+					if (aboveCell.getDown() != CellComponents.WALL) {
+						currentRow = aboveRow;
+						currentCell = aboveCell;
+						moved = true;
+					}
+				}
+				break;
+
+			case DOWN:
+				if (rowIndex < grid.getRows().size() - 1) {
+					Row belowRow = grid.getRows().get(rowIndex + 1);
+					Cell belowCell = belowRow.getCells().get(currentIndex);
+					if (belowCell.getUp() != CellComponents.WALL) {
+						currentRow = belowRow;
+						currentCell = belowCell;
+						moved = true;
+					}
+				}
+				break;
+
+			case LEFT:
+				if (currentCell.getLeft() != CellComponents.WALL && currentIndex > 0) {
+					currentCell = currentRow.getCells().get(currentIndex - 1);
 					moved = true;
 				}
-			}
-			break;
-			
-		/**
-		* Checks if the right of the Cell has a space
-		* Then it checks the position of the player to see if it's on the very right
-		* If it is on the very right wall it can't more right again
-		* If not it can move right by moving up one on the index
-		*/			
-		case RIGHT:
-			if (currentCell.getRight() != CellComponents.WALL) {
-				if(currentIndex < currentRow.getCells().size() - 1) {
+				break;
+
+			case RIGHT:
+				if (currentCell.getRight() != CellComponents.WALL && currentIndex < currentRow.getCells().size() - 1) {
 					currentCell = currentRow.getCells().get(currentIndex + 1);
+					moved = true;
 				}
-			}
-			break;
-			
+				break;
 		}
-		//Just to give a invalid move message to the player
-		if (moved == false) {
+
+		if (!moved) {
 			System.out.println("Invalid Move");
 		}
-		
+
 		return moved;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Player [currentCell=" + currentCell + ", currentRow=" + currentRow + "]";
 	}
 }
+
