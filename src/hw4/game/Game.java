aa39e088e98a547b509e5cb5729dd690bb32ab81
Son <Son@MSI>
2025-04-22 17:17:55 -0500

    starts on Game.java

--- src/hw4/game/Game.java
+++ src/hw4/game/Game.java
@@ -1,5 +1,25 @@
 package hw4.game;
+import hw4.player.Movement;
+import hw4.maze.Grid;
+import hw4.player.Player;
 
 public class Game {
+    private final Grid grid;
+    private final Player player;
+    private boolean isGameOver;
 
+    public Game(Grid grid, Player player) {
+        this.grid = grid;
+        this.player = player;
+        this.isGameOver = false;
+    }
+
+    public void movePlayer(String direction) {
+        if (isGameOver) return;
+        {}}
+      
+
+    public boolean isOver() {
+        return isGameOver;
+    }
 }
